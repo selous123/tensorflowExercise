5da59ce463c01c0629cd03a0dd97fd3b0cf29dec
lrhselous@163.com <lrhselous@163.com>
2017-08-18 15:18:37 +0800

    coding

--- Generative Adversarial Nets/generateMnist.py
+++ Generative Adversarial Nets/generateMnist.py
@@ -4,6 +4,7 @@ import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
 import argparse
 import sys
+import cv2
 FLAGS = None
 
 def Generator(z):
@@ -17,7 +18,7 @@ def Generator(z):
     with tf.name_scope("G_nn1"):
         weight = tf.Variable(tf.truncated_normal(shape = [100,128],stddev=0.1),name = "G_weight",dtype="float32")
         bias = tf.Variable(tf.constant(0.1,shape=[128]),name = "G_bias")
-        G_nn1_output = tf.nn.relu(tf.matmul(z,weight)+bias,name = "G_nn1_output")
+        G_nn1_output = tf.nn.tanh(tf.matmul(z,weight)+bias,name = "G_nn1_output")
         theta_G.append(weight)
         theta_G.append(bias)
     with tf.name_scope("G_nn2"):
@@ -31,7 +32,7 @@ def Generator(z):
 def Discriminator(x,reuse=None):
     """
     args:
-        x:input image,size:(784,)
+        x:input image,size:(batch_size,784)
     return:
         prob and logits
     """
@@ -39,7 +40,7 @@ def Discriminator(x,reuse=None):
     with tf.variable_scope("D_nn1",reuse=reuse):
         weight = tf.get_variable(name="D_weight",initializer = tf.truncated_normal_initializer(stddev=0.1),shape=[784,128])
         bias = tf.get_variable(name = "D_bias",initializer = tf.constant_initializer(0.1),shape=[128])
-        D_nn1_output = tf.nn.relu(tf.matmul(x,weight)+bias,name ="D_nn1_output")
+        D_nn1_output = tf.nn.tanh(tf.matmul(x,weight)+bias,name ="D_nn1_output")
         theta_D.append(weight)
         theta_D.append(bias)
 
@@ -67,31 +68,50 @@ def training():
     D_fake,D_logit_fake,_= Discriminator(G_sample,reuse=True)
 
     #loss function
-    D_loss = -tf.reduce_mean(tf.log(D_real)+tf.log(1. -D_fake))
-    G_loss = -tf.reduce_mean(tf.log(D_fake))
-
-
+    D_loss = -tf.reduce_mean(tf.log(D_logit_real)+tf.log(1. -D_logit_fake))
+    G_loss = -tf.reduce_mean(tf.log(D_logit_fake))
+    
+    tf.summary.scalar("D_loss",D_loss)
+    tf.summary.scalar("G_loss",G_loss)
     #optimizer
     D_optimizer = tf.train.AdamOptimizer().minimize(D_loss, var_list=theta_D)
     G_optimizer = tf.train.AdamOptimizer().minimize(G_loss, var_list=theta_G)
-
+    merged_summary_op = tf.summary.merge_all()
+    
     session = tf.Session()
     session.run(tf.global_variables_initializer())
-
+    if tf.gfile.Exists(FLAGS.log_dir):
+        tf.gfile.DeleteRecursively(FLAGS.log_dir);
+    summary_writer = tf.summary.FileWriter(FLAGS.log_dir,session.graph)
     z_sample = sample([FLAGS.batch_size,100]);
     for step in range(FLAGS.max_steps):
         x_input,_ = data_set.train.next_batch(FLAGS.batch_size)
-
-        _,D_loss_curr = session.run([D_optimizer,D_loss],feed_dict ={
-            x:x_input,
-            z:z_sample
-            })
-        _,G_loss_curr = session.run([G_optimizer,G_loss],feed_dict ={
-            z:z_sample
-            })
+        
+        for d_step in range(FLAGS.d_steps):
+            _,D_loss_curr,D_real_curr = session.run([D_optimizer,D_loss,D_logit_fake],feed_dict ={
+                x:x_input,
+                z:z_sample
+                })
+        for g_step in range(FLAGS.g_steps):
+            _,G_loss_curr = session.run([G_optimizer,G_loss],feed_dict ={
+                z:z_sample
+                })
         if step%1000==0:
             print "step:{},D_loss:{},G_loss:{}".format(step,D_loss_curr,G_loss_curr)
-
+            summary_str = session.run(merged_summary_op,feed_dict={x: x_input, z:z_sample});
+            summary_writer.add_summary(summary_str, step);
+            #print D_real_curr
+        if step%1000 == 0:
+            G_sample_curr = session.run([G_sample],feed_dict={
+                    z:z_sample
+                    })
+            img = np.array(G_sample_curr).reshape(-1,28,28)
+            img_dir = str(step/1000)
+            if tf.gfile.Exists(img_dir):
+                tf.gfile.DeleteRecursively(img_dir);
+            tf.gfile.MakeDirs(img_dir);
+            for ind in range(FLAGS.batch_size):
+                cv2.imwrite(img_dir+"/"+str(ind)+".jpg",img[ind]*255)
 def main(_):
     training()
 
@@ -103,7 +123,7 @@ if __name__=="__main__":
     parser.add_argument(
         '--input_data_dir',
         type = str,
-        default = "data/mnist",
+        default = "/mnt/hgfs/ubuntu14/dataset/mnist",
         help = "directory of mnist data"
         )
 
@@ -117,8 +137,27 @@ if __name__=="__main__":
     parser.add_argument(
         '--max_steps',
         type=int,
-        default=10000,
+        default=100000,
         help='max steps'
+        )
+    
+    parser.add_argument(
+        '--g_steps',
+        type=int,
+        default=2,
+        help='Generator steps'
+        )
+    parser.add_argument(
+        '--d_steps',
+        type=int,
+        default=1,
+        help='Discriminator steps'
+        )
+    parser.add_argument(
+        '--log_dir',
+        type=str,
+        default="/tmp/gan/log",
+        help='the log directory'
         )
 
     FLAGS, unparsed = parser.parse_known_args()
